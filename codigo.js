9e8489eb6ebef3377692aa48b5d4c86198e58760
nacho1g <nachogalvez1@gmail.com>
2023-07-24 12:25:59 -0300

    PreEntrega2

--- codigo.js
+++ codigo.js
@@ -1,20 +1,43 @@
-let edad, fecha = 2023, nombre ="";
-edad = parseInt(prompt("Ingrese su edad"));
-fecha -= edad;
-console.log(fecha);
-if(edad < 18){
-    alert("Usted es menor de edad");
+let turnos = [
+    {
+        nombre: "Juan",
+        apellido: "Gonzalez",
+        edad: 34
+    },
+    {
+        nombre: "Maria",
+        apellido: "Alvarez",
+        edad: 57
+    },
+    {
+        nombre: "Agustin",
+        apellido: "Lopez",
+        edad: 21
+    },
+];
+function Turno(nombre, apellido, edad){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this. edad = edad;
 }
-else{
-    let turno = 10;
-    alert("Probablemente nacio en "+ fecha) ;
-    nombre = prompt("Ingrese su nombre");
-    console.log(nombre);
-    alert(nombre+", su turno es el "+turno+" espere a ser atendido.");
-    for(contar =0; contar < turno; contar++){
-        console.log(contar);
-    }
-    alert("Siguente turno "+ turno);
 
+name = prompt("Ingrese su nombre");
+
+lastname = prompt("Ingrese su apellido");
+age = parseInt(prompt("Ingrese su edad"));
+while( Number.isNaN(age) == true){
+    age = parseInt(prompt("No ingresaste un numero"));
 }
+
+turn = new Turno(name,lastname,age);
+turnos.push(turn);
+
+turnos.forEach(element => console.log(element));
+
+for(let i = 0; i< turnos.length; i =0){
+    alert("Es el turno de " + turnos[i].nombre);
+    turnos.shift();
+}
+
+
 
